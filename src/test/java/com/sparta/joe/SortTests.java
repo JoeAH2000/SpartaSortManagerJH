9a6e0660b1f7a1094941f40b220023311cb9bfb6
JoeAH2000 <joehilton00@gmail.com>
2021-08-16 16:46:08 +0100

    Added Sort Tests for all sorts

--- src/test/java/com/sparta/joe/SortTests.java
+++ src/test/java/com/sparta/joe/SortTests.java
@@ -1,7 +1,11 @@
 package com.sparta.joe;
 
+import com.sparta.joe.sorters.binarytreesort.BinaryTreeSort;
 import com.sparta.joe.sorters.bubblesort.BubbleSort;
 import com.sparta.joe.sorters.insertionsort.InsertionSort;
+import com.sparta.joe.sorters.mergesort.MergeSort;
+import com.sparta.joe.sorters.quicksort.QuickSort;
+import com.sparta.joe.sorters.selectionsort.SelectionSort;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +24,37 @@ public class SortTests {
         int[] unsortedArray = {3, 5, 2, 4, 1};
         int[] sortedArray = {1, 2, 3, 4, 5};
         Assertions.assertArrayEquals(sortedArray, insertionSort.sortArray(unsortedArray));
+    }
+
+    @Test
+    void mergeSortTest() {
+        MergeSort mergeSort = new MergeSort();
+        int[] unsortedArray = {3, 5, 2, 4, 1};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        Assertions.assertArrayEquals(sortedArray, mergeSort.sortArray(unsortedArray));
+    }
+
+    @Test
+    void quickSortTest() {
+        QuickSort quickSort = new QuickSort();
+        int[] unsortedArray = {3, 5, 2, 4, 1};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        Assertions.assertArrayEquals(sortedArray, quickSort.sortArray(unsortedArray));
+    }
+
+    @Test
+    void selectionSortTest() {
+        SelectionSort selectionSort = new SelectionSort();
+        int[] unsortedArray = {3, 5, 2, 4, 1};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        Assertions.assertArrayEquals(sortedArray, selectionSort.sortArray(unsortedArray));
+    }
+
+    @Test
+    void binaryTreeSortTest() {
+        BinaryTreeSort binaryTreeSort = new BinaryTreeSort();
+        int[] unsortedArray = {3, 5, 2, 4, 1};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        Assertions.assertArrayEquals(sortedArray, binaryTreeSort.sortArray(unsortedArray));
     }
 }
